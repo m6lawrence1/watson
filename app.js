46816c6b6ca69f8925d3d6da4dc7e7b474bcc6a6
Jessica Dafforn <jdafforn072@gmail.com>
2016-05-07 15:50:50 -0400

    updated app

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
-/**
- * Copyright 2015 IBM Corp. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the 'License');
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+var watson = require('watson-developer-cloud');
 
-'use strict';
+var personality_insights = watson.personality_insights({
+  username: '40e862ed-0b2a-48a5-87b9-be1d2d37e63b',
+  password: 'lSbzo8yacgjP',
+  version: 'v2'
+});
 
-require('./helpers/env.js');
-
-var express = require('express'),
-    app     = express();
-
-// Bootstrap application settings
-require('./config/express')(app);
-
-// Add routes to app
-require('./router')(app);
-
-// error-handler settings
-require('./config/error-handler')(app);
-
-module.exports = app;
+personality_insights.profile({
+  text: 'Call me Ishmael. Some years ago-never mind how long precisely-having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking peoples hats off-then, I account it high time to get to sea as soon as I can.',
+  language: 'en' },
+  function (err, response) {
+    if (err)
+      console.log('error:', err);
+    else
+      console.log(JSON.stringify(response, null, 2));
+});
