99353837e6925b451378a5165c598e26b86bc8c0
Jessica Dafforn <jdafforn072@gmail.com>
2016-05-11 23:11:55 -0400

    attempted refining Get request

--- public/formCtrl.js
+++ public/formCtrl.js
@@ -1,13 +1,23 @@
 var app = angular.module('myModule');
 
-app.controller('formCtrl', ['$scope', 'watsonService', function ($scope, watsonService) {
-    console.log('num1');
-    
-	$scope.personalitize = function () {
-		var inputText = $scope.text;
-        console.log('hello');
-    }
-    watsonService(inputText);
+// app.controller('formCtrl', ['$scope', 'watsonService', function ($scope, watsonService) {
+//     console.log('num1');
 
+app.controller('formCtrl',function(
+    on('click', personalitize(){
+        $http({
+         method: 'GET',
+         url: '../index.js'
+     }))
+         .then(function successCallback(response) {
+             var profileOutput = myService.valueSetter();
+             $scope.xx = profileOutput.xx;
+             $scope.xx = profileOutput.xx;
+             $scope.xx = profileOutput.xx;
+      }, function errorCallback(response) {
+        alert('error');
+))
+  })
+ });
     console.log('this is the form controller');
 }]);
