7973edb680e19a80a0c87bf37b9f2a28d408a154
Michael Lawrence <m6lawrence1@gmail.com>
2016-05-11 21:05:50 -0400

    updated something

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-var express = require('express'); 
+//var express = require('express'); 
 var watson = require('watson-developer-cloud');
-var service = require('./service');
-var app = express();
+//var service = require('./service');
+//var app = express();
 
 var personality_insights = watson.personality_insights({
-  username: '40e862ed-0b2a-48a5-87b9-be1d2d37e63b',
-  password: 'lSbzo8yacgjP',
+  username: '0c8ddde4-e322-4a1a-a521-511cd47afb84',
+  password: 'q2n2Za3Dbwai',
   version: 'v2'
 });
 
-//var my_profile = "Call me Ishmael. Some years ago-never mind how long precisely-having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off-then, I account it high time to get to sea as soon as I can.";
+var my_profile = "Call me Ishmael. Some years ago-never mind how long precisely-having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off-then, I account it high time to get to sea as soon as I can. Call me Ishmael. Some years ago-never mind how long precisely-having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off-then, I account it high time to get to sea as soon as I can. Call me Ishmael. Some years ago-never mind how long precisely-having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off-then, I account it high time to get to sea as soon as I can.";
 
 personality_insights.profile({ text: my_profile },
 function (err, profile) {
   if (err)
-    console.log(err)
+    console.log(err);
   else
     console.log(profile);
 });
 
-var server = app.listen(3000, function () { 
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("fml");
-});
-    
-app.use(express.static(__dirname + '/public'));
-
+//app.get('/api/lyric', function (req, res) {  
+// res.send(store[Math.floor(Math.random()*store.length)]);
+//});
+//
+//
+//var server = app.listen(3000, function () { 
+//    var host = server.address().address;
+//    var port = server.address().port;
+//    console.log("fml");
+//});
+//    
+//app.use(express.static(__dirname + '/public'));
+//
 
