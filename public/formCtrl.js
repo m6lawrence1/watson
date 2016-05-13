a48a1c60261be91d6358f51b27ba3fea5ef23e7d
Jessica Dafforn <jdafforn072@gmail.com>
2016-05-13 08:16:08 -0400

    added code to display results on partial. Unsuccessful so far

--- public/formCtrl.js
+++ public/formCtrl.js
@@ -1,18 +1,28 @@
 var app = angular.module('myModule');
 
+// app.controller('formCtrl', function ($scope, $http){
+//     $scope.personalitize = function () {
+//         $http.post('/api', {"body":$scope.textInput})
+//          .then(function successCallback(response) {
+//             var profileOutput = profile.response;
+//             $scope.id = profile;
+//             console.log('profileOutput');
+//
+//         }, function errorCallback(response) {
+//         alert('error');
+//         });
+//   };
+// });
 app.controller('formCtrl', function ($scope, $http){
     $scope.personalitize = function () {
         $http.post('/api', {"body":$scope.textInput})
          .then(function successCallback(response) {
-//             var profileOutput = myService.valueSetter();
-//             $scope.xx = profileOutput.xx;
-            console.log("response");
+            $scope.children = response;
 
         }, function errorCallback(response) {
         alert('error');
         });
   };
 });
-
 //    console.log('this is the form controller');
 //}]);
