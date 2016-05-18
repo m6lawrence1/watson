3b702b0456d865c9dcec1e230b842b78de18271f
Michael Lawrence <m6lawrence1@gmail.com>
2016-05-17 20:53:52 -0400

    nested results

--- public/formCtrl.js
+++ public/formCtrl.js
@@ -9,15 +9,16 @@ app.controller('formCtrl', function ($scope, $http){
         .then(function(response) {
             console.log(response);
             $scope.big5=response.data.tree.children[0].children[0].children;
-            $scope.openness=response.data.tree.children[0].children[0].children[0].children;
-            $scope.conscientiousness=response.data.tree.children[0].children[0].children[1].children;
-            $scope.extraversion=response.data.tree.children[0].children[0].children[2].children;
-            $scope.agreeableness=response.data.tree.children[0].children[0].children[3].children;
-            $scope.emotional=response.data.tree.children[0].children[0].children[4].children;
+//            $scope.openness=response.data.tree.children[0].children[0].children[0].children;
+//            $scope.conscientiousness=response.data.tree.children[0].children[0].children[1].children;
+//            $scope.extraversion=response.data.tree.children[0].children[0].children[2].children;
+//            $scope.agreeableness=response.data.tree.children[0].children[0].children[3].children;
+//            $scope.emotional=response.data.tree.children[0].children[0].children[4].children;
+//            $scope.
             
-
         }, function errorCallback(response) {
         alert('error');
         });
+        
   };
 });
